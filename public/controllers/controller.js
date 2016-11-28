a51b2edc4f5a4b1ee2bea9152a98a27f88fe0b4d
Vadim Kovalenko <vadblaxmith@gmail.com>
2016-11-28 22:37:09 +0200

    Added bootstrap accordion

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -7,9 +7,6 @@ Colortask1.controller('Colortask1Ctrl', ['$scope', '$http', function($scope, $ht
 		$http.get('/colortask_1').success(function(response) {
 			console.log("I got the data I requested");
 			$scope.colortask_1 = response;
-   		//Установка по умолчанию
-   		//$scope.activeItem = $scope.proj_item;
-   		//$scope.activeItem = $scope.colortask_1[0];
    		$scope.proj = "";
 		});
 	}
@@ -56,14 +53,6 @@ Colortask1.controller('Colortask1Ctrl', ['$scope', '$http', function($scope, $ht
 	};*/
 
 	$scope.addNewProject = function() {
-		/*console.log("HELLO, " + $scope.name);
-		$http.post('/new', $scope.name).success(function(response) {
-			$scope.name = response;
-			console.log("Hi from post-controller");*/
-			/*$http.get('/new', $scope.name).success(function(response) {
-				console.log(response);
-			});*/
-
 			$http.post('/colortask_1', $scope.proj).success(function(response) {
 			console.log(response);
 			refresh();	
@@ -75,34 +64,15 @@ Colortask1.controller('ProjCtrl', ['$scope', '$http', function($scope, $http) {
 
 $scope.setActive = function(proj_item) {
 		$scope.activeItem = proj_item;
-		//console.log($scope.activeMenu.data);
 	};
-
-	//$scope.activeItem = $scope.colortask_1[0].colors[0];
-	//console.log($scope.colortask_1[0].colors[0]);
-
-	/*$scope.colortask_1.map(function() {
-		$scope.activeItem = $scope.colortask_1[i].colors[0];
-	});*/
 
 	console.log($scope.colortask_1.length);
 	console.log($scope.colortask_1[0].colors[0]);
 
+	//Set default item
 	if ($scope.proj.colors) {
 		$scope.activeItem = $scope.proj.colors[0];
 	}
-
-	/*for(i=0; i < $scope.colortask_1.length; ++i) {
-		//Take a count of projects
-		//console.log($scope.colortask_1.length);
-		console.log("Hello");
-		for (j=0; j < $scope.colortask_1[i].colors.length; ++j) {
-			//Take a count of colors in each project
-				console.log($scope.colortask_1[i].colors[0].data);
-				return $scope.activeItem = $scope.colortask_1[j].colors[0];
-		};
-	};*/
-
 
 }]);	
 
